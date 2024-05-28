e456e0135cefdcf7a7e05288bb126ff228ebed47
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-28 23:23:59 +0530

    commit-2-o

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,11 @@
 
 A
 src/MainForm.fxml,1\a\1a4689d1e1682d7ee44faeda1a337e667ccebe0e
+K
+src/MainFormController.java,7\7\7725cb156b831ce9623a23c75bc06069630a3ada
+G
+src/Appinitializer.java,e\3\e31c3cd7825f02afe105aeefb748639943ed056a
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,2 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colDelete;
+    public TableColumn colUpdate;
+
+    public void btnSubmit(ActionEvent actionEvent) {
+    }
 }
