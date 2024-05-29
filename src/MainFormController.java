44872b5887560062a591435b2f9ea1455d2668b8
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-29 22:14:53 +0530

    commit-5-o

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,7 +1,8 @@
+import entity.Student;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class MainFormController {
     public TextField txtId;
@@ -13,9 +14,41 @@ public class MainFormController {
     public TableColumn colAddress;
     public TableColumn colDelete;
     public TableColumn colUpdate;
+    public Button btn;
 
     public void btnSubmit(ActionEvent actionEvent) {
+        Student student = new Student(Long.parseLong(txtId.getText()),txtName.getText(),txtAddress.getText());
+        if (btn.getText().equals("Save Student")){
+            //save
+            try(Session session = HibernateUtil.getSession()){
+                Transaction transaction = session.beginTransaction();
+                session.save(student);
+                transaction.commit();
+                loadall();
+                new Alert(Alert.AlertType.INFORMATION,"Saved.....!").show();
+            }
+        }else{
+            //update
+            try(Session session = HibernateUtil.getSession()){
+                Transaction transaction = session.beginTransaction();
+                Student selectedStudent = session.get(Student.class, student.getId());
+                if (selectedStudent==null){
+                    new Alert(Alert.AlertType.WARNING,"Student not found....!");
+                    return;
+                }
+                selectedStudent.setName(student.getName());
+                selectedStudent.setAddress(student.getAddress());
+
+                transaction.commit();
+                loadall();
+                new Alert(Alert.AlertType.INFORMATION,"Updated......!").show();
+            }
+        }
     }
+
+    private void loadall() {
+    }
+
     public void initialize(){
         HibernateUtil.getSession();
     }
